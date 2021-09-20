a2a016fc5d1394cbbcab276a3432c85c7ffadf0c
James <jameschen625@gmail.com>
2021-09-20 10:05:59 -0400

    backend user auth

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const passport = require('passport');
+
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
+
+app.use("/api/users", users);
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
