5af39c50a253576e0ce8a7841881c2d4ad099110
James <jameschen625@gmail.com>
2021-09-21 19:37:38 -0400

    heroku push

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ const passport = require('passport');
 const http = require("http");
 const server = http.createServer(app);
 const socketio = require('socket.io');
-const io = socketio(server)
+const io = socketio(server);
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true })
