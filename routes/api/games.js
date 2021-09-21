c6e1caea0f35acb7ecfbb3fb10de1d5efef13030
Wajahat Shoukat <wajahatshoukat@gmail.com>
2021-09-21 11:11:37 -0400

    fixed game routes

--- routes/api/games.js
+++ routes/api/games.js
@@ -4,35 +4,34 @@ const router = express.Router();
 const Game = require('../../models/Game');
 const Lobby = require('../../models/Lobby');
 
-// router.get('/', (req, res) => {
-//     Game.find()
-//         .sort({ date: -1 })
-//         .then(games => res.json(games))
-//         .catch(err => res.status(404).json({ nogamesfound: 'No games found' }));
-// });     
+router.get('/', (req, res) => {
+    Game.find()
+        .sort({ date: -1 })
+        .then(games => res.json(games))
+        .catch(err => res.status(404).json({ nogamesfound: 'No games found' }));
+});     
 
-router.get('/', async(req, res) => {
-    
+
+
+// router.get('/:id', (req, res) => {
+//     Game.findById(req.params.id)
+//         .then(game => res.json(game))
+//         .catch(err =>
+//             res.status(404).json({ nogamefound: 'No game' })
+//         );
+// });
+
+router.get('/:id', async(req, res) => {
+   
     try {
-        const games = await Game.find();
-        console.log(games);
-        const lobbies = await Promise.all(games.lobbies.map(lobbyId => Lobby.findById(lobbyId)));
-        console.log(1);
-        res.json({games, lobbies});
+        const game = await Game.findById(req.params.id)
+        console.log(game);
+        const lobbies = await Promise.all(game.lobbies.map(lobbyId => Lobby.findById(lobbyId)));
+        res.json({game, lobbies});
     }        
     catch {
-        res.status(404).json({ nogamesfound: 'No games found' })
+        res.status(404).json({ nogamefound: 'No game' })
     }
 });
-
-
-router.get('/:id', (req, res) => {
-    Game.findById(req.params.id)
-        .then(game => res.json(game))
-        .catch(err =>
-            res.status(404).json({ nogamefound: 'No game' })
-        );
-});
-
 
 module.exports = router;
