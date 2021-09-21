153c6b28760ac45af751f969f22dfead5d4ab170
JFlec <josephflec@gmail.com>
2021-09-21 11:52:18 -0500

    auth decal

--- frontend/src/components/form/form.jsx
+++ frontend/src/components/form/form.jsx
@@ -57,30 +57,34 @@ class Form extends React.Component {
     if (this.props.type === 'signin') {
       return (
         <form onSubmit={this.handleSubmit('signin')}>
-          <div className="login-container">
-            <h1>Login to get started</h1>
-            <div className="login-input-container">
-              <input
-                type="text"
-                value={this.state.email}
-                onChange={this.input('email')}
-                className="login-input one"
-                placeholder=" "
-                autoFocus
-              />
-              <span className="placeholder-one">Email</span>
-              <input
-                type="text"
-                value={this.state.password}
-                onChange={this.input('password')}
-                className="login-input two"
-                placeholder=" "
-              />
-              <span className="placeholder-two">Password</span>
-              <p className="login-redirect-text">
-                Don't have an account? <sign-up>Sign up</sign-up>
-              </p>
-              <button className="auth-button">Login</button>
+          <div className="god-container">
+            <div className="splash-art"></div>
+            <div className="splash-art-dark"></div>
+            <div className="login-container">
+              <h1>Login to get started</h1>
+              <div className="login-input-container">
+                <input
+                  type="text"
+                  value={this.state.email}
+                  onChange={this.input('email')}
+                  className="login-input one"
+                  placeholder=" "
+                  autoFocus
+                />
+                <span className="placeholder-one">Email</span>
+                <input
+                  type="password"
+                  value={this.state.password}
+                  onChange={this.input('password')}
+                  className="login-input two"
+                  placeholder=" "
+                />
+                <span className="placeholder-two">Password</span>
+                <p className="login-redirect-text">
+                  Don't have an account? <sign-up>Sign up</sign-up>
+                </p>
+                <button className="auth-button">Login</button>
+              </div>
             </div>
           </div>
         </form>
@@ -88,43 +92,47 @@ class Form extends React.Component {
     } else if (this.props.type === 'signup') {
       return (
         <form onSubmit={this.handleSubmit('signup')}>
-          <div className="signup-container">
-            <h1>Become a Wajer!</h1>
-            <div className="signup-input-container">
-              <input
-                type="text"
-                value={this.state.email}
-                onChange={this.input('email')}
-                className="signup-input one"
-                placeholder=" "
-                autoFocus
-              />
-              <span className="placeholder-one">Email</span>
-              <input
-                type="text"
-                value={this.state.username}
-                onChange={this.input('username')}
-                className="signup-input two"
-                placeholder=" "
-              />
-              <span className="placeholder-two">Username</span>
-              <input
-                type="password"
-                value={this.state.password}
-                onChange={this.input('password')}
-                className="signup-input three"
-                placeholder=" "
-              />
-              <span className="placeholder-three">Password</span>
-              <input
-                type="password"
-                value={this.state.password2}
-                onChange={this.input('password2')}
-                className="signup-input four"
-                placeholder=" "
-              />
-              <span className="placeholder-four">Confirm Password</span>
-              <button className="auth-button">Sign up</button>
+          <div className="god-container">
+            <div className="splash-art"></div>
+            <div className="splash-art-dark"></div>
+            <div className="signup-container">
+              <h1>Become a Wajer!</h1>
+              <div className="signup-input-container">
+                <input
+                  type="text"
+                  value={this.state.email}
+                  onChange={this.input('email')}
+                  className="signup-input one"
+                  placeholder=" "
+                  autoFocus
+                />
+                <span className="placeholder-one">Email</span>
+                <input
+                  type="text"
+                  value={this.state.username}
+                  onChange={this.input('username')}
+                  className="signup-input two"
+                  placeholder=" "
+                />
+                <span className="placeholder-two">Username</span>
+                <input
+                  type="password"
+                  value={this.state.password}
+                  onChange={this.input('password')}
+                  className="signup-input three"
+                  placeholder=" "
+                />
+                <span className="placeholder-three">Password</span>
+                <input
+                  type="password"
+                  value={this.state.password2}
+                  onChange={this.input('password2')}
+                  className="signup-input four"
+                  placeholder=" "
+                />
+                <span className="placeholder-four">Confirm Password</span>
+                <button className="auth-button">Sign up</button>
+              </div>
             </div>
           </div>
         </form>
