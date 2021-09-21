de080cab3fed7b3a88219a4dccba8a4ab2ba4cbf
Wajahat Shoukat <wajahatshoukat@gmail.com>
2021-09-21 10:32:27 -0400

    fixed routes

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import GameReducer from './game_reducer';
+import lobbysReducer from './lobbys_reducer';
 import messagesReducer from './messages_reducer';
 
 const entitiesReducer = combineReducers({
     games: GameReducer,
-    messages: messagesReducer
+    messages: messagesReducer,
+    lobbies: lobbysReducer
 })
 
 export default entitiesReducer;
