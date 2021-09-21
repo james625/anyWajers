19187bbdbe3f88ad5bbba313430d3af17a838454
James <jameschen625@gmail.com>
2021-09-21 19:41:02 -0400

    heroku push

--- routes/api/games.js
+++ routes/api/games.js
@@ -12,31 +12,31 @@ router.get('/', (req, res) => {
     .catch((err) => res.status(404).json({ nogamesfound: 'No games found' }));
 });
 
-// router.get('/:id', (req, res) => {
-//     Game.findById(req.params.id)
-//         .then(game => res.json(game))
-//         .catch(err =>
-//             res.status(404).json({ nogamefound: 'No game' })
-//         );
-// });
-
-router.get('/:id', async (req, res) => {
-  try {
-    const game = await Game.findById(req.params.id);
-    game.lobbies = await Promise.all(
-      game.lobbies.map(async (lobbyId) => {
-        const lobby = await Lobby.findById(lobbyId);
-        const banana = await Promise.all(
-          lobby.players.map((id) => User.findById(id))
+router.get('/:id', (req, res) => {
+    Game.findById(req.params.id)
+        .then(game => res.json(game))
+        .catch(err =>
+            res.status(404).json({ nogamefound: 'No game' })
         );
-        console.log(banana);
-        return lobby;
-      })
-    );
-    res.json(game);
-  } catch {
-    res.status(404).json({ nogamefound: 'No game' });
-  }
 });
+
+// router.get('/:id', async (req, res) => {
+//   try {
+//     const game = await Game.findById(req.params.id);
+//     game.lobbies = await Promise.all(
+//       game.lobbies.map(async (lobbyId) => {
+//         const lobby = await Lobby.findById(lobbyId);
+//         const banana = await Promise.all(
+//           lobby.players.map((id) => User.findById(id))
+//         );
+//         console.log(banana);
+//         return lobby;
+//       })
+//     );
+//     res.json(game);
+//   } catch {
+//     res.status(404).json({ nogamefound: 'No game' });
+//   }
+// });
 
 module.exports = router;
