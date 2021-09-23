fb827cc883d000a1c39bc28d90d70c7f7ad6943c
cgradeff <64032507+cgradeff@users.noreply.github.com>
2021-09-22 21:37:56 -0400

    need to fix user edit and add delete

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -9,11 +9,11 @@ import '../assets/splash/splash.scss';
 
 import NavBar from './nav_bar/nav_bar';
 import ModalContainer from './modal/modal_container';
-// import SplashContainer from './splash/splash_container';
 import GamesIndexContainer from './games/games_index_container';
 import GameShowContainer from './games/game_show_container';
 import MessagesContainer from './messages/messages_container';
 import LobbyShowContainer from './lobbies/lobby_show_container';
+import UserContainer from './user/user_container';
 
 const App = () => (
   <div>
@@ -21,10 +21,10 @@ const App = () => (
     <NavBar />
     <Switch>
       <Route exact path="/" component={GamesIndexContainer} />
-      {/* <Route exact path="/games" component={GamesIndexContainer} /> */}
       <Route exact path="/games/:gameId" component={GameShowContainer} />
       <Route exact path="/games/:gameId/:lobbyId" component={LobbyShowContainer} />
       <Route exact path="/messages" component={MessagesContainer} />
+      <Route exact path="/users/:userId" component={UserContainer} />
     </Switch>
   </div>
 );
---
+++ frontend/src/components/user/user.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+
+class User extends React.Component {
+  constructor(props) {
+    super(props);
+    this.bio = '';
+    this.props.currentUser.bio === undefined ? this.bio = '' : this.bio = this.props.currentUser.bio
+    this.state = {
+      email: this.props.currentUser.email,
+      username: this.props.currentUser.username,
+      bio: this.bio,
+      edit: false
+    };
+    this.update = this.update.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  update(field) {
+    return (e) => this.setState({ [field]: e.currentTarget.value })
+  }
+
+  onClick(e) {
+    e.preventDefault()
+    this.setState({
+      edit: true
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    let user = {
+      email: this.state.email,
+      username: this.state.username,
+      bio: this.state.bio,
+      id: this.props.currentUser.id
+    }
+    this.props.editUser(user)
+    this.setState({
+      edit: false
+    })
+  }
+
+  render() {
+    debugger
+    const edit = () => {
+      return (
+        <div>
+          <h2>{this.state.email}</h2>
+          <form action="">
+            <input type='text' 
+            value={this.state.username}
+            onChange={this.update('username')} />
+            <input type='text'  
+            value={this.state.bio}
+            onChange={this.update('bio')}/>
+            <input type="submit" value="Submit" onClick={this.handleSubmit} />
+          </form>
+          
+        </div>
+      )
+    }
+
+    const show = () => {
+      return (
+        <div>
+          <h2>{this.state.email}</h2>
+          <p>{this.state.username}</p>
+          <p>{this.state.bio}</p>
+          <button onClick={this.onClick}>Edit</button>
+        </div>
+      )
+    }
+
+    return (this.state.edit ? edit() : show())
+  }
+}
+
+export default User;
